9adf326b9685377bf3f705e98e2ff5dda6123340
Rasmus S <rasmus.salmela@gmail.com>
2023-08-13 16:27:48 +0300

    Add login and register

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -10,4 +11,14 @@ def index(request):
     return render(request, 'index.html')
 
 def register(request):
-    print()
+    
+    if request.method == "GET":
+        return render(request, 'register.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        admin = request.POST.get('type')
+
+        User.objects.create_user(username=username, password=password, is_staff=admin)
+
+        return redirect('/chat/')
